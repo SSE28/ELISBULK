575f108c58d6a50566035d027c3d9511e03f0036
SSE28 <zs19022226@estudiantes.uv.mx>
2023-06-04 18:24:08 -0600

    SERVLET INICIO SESION

--- src/main/java/Controlador/SVinicio.java
+++ src/main/java/Controlador/SVinicio.java
@@ -1,2 +1,27 @@
-package Controlador;public class SVinicio {
+package Controlador;
+import Data.usuariosDAO;
+import Modelo.usuariosJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "SVinicio", value = "/SVinicio")
+public class SVinicio extends HttpServlet {
+
+    usuariosJB usuariosJB =new usuariosJB();
+
+    usuariosDAO usuariosDAO = new usuariosDAO();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("entra al ");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }
